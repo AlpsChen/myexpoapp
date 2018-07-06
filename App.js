628daadd6f3648b349a88136d1b55fc78b2a2e16
Lin Pin Chueh <javck.zack@gmail.com>
2018-07-06 18:51:58 +0800

    解決衝突

--- App.js
+++ App.js
@@ -6,28 +6,17 @@ import {
 } from 'react-native';
 
 export default class App extends React.Component {
-<<<<<<< Updated upstream
   //bug修好了
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+    return ( <
+      View style = {
+        styles.container
+      } >
+      <
+      Text > Hello World! < /Text> <
+      /View>
     );
   }
-=======
-  //登入功能
-  //改bug2
-  return ( <
-    View style = {
-      styles.container
-    } >
-    <
-    Text > Hello World! < /Text> <
-    /View>
-  );
-}
->>>>>>> Stashed changes
 }
 
 const styles = StyleSheet.create({
